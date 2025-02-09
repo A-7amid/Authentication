ed6bb77dea8bf80c66cc01fa0817e0aec6a7be0c
tobi <abdelhameda410@gmail.com>
2025-02-09 23:20:13 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Home from "./Components/Home";
 import LoginPage from "./Components/LoginPage";
 import SignUpPage from "./Components/SignUpPage";
 import ResetPasswordPage from "./Components/ResetPasswordPage";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} errorElement={<NotFound />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/resetpassword" element={<ResetPasswordPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
